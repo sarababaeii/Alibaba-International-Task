0bd126725fb39b22956ea69db472e583878d14a1
Sara Babaei <sarababaei212@gmail.com>
2022-02-05 19:57:00 +0330

    Initial Commit

---
+++ Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task
+//
+//  Created by Sara Babaei on 2/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
